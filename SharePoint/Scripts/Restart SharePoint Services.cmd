805759c1f3124583dd81e6b69d7e86a6d9128846
Jeremy Jameson <jeremy_jameson@hotmail.com>
2013-05-24 15:54:02 -0600

    Combine script for restarting SharePoint
    Evidently I never blogged about the fact that I combined the scripts to
    support both MOSS 2007 and SharePoint 2010

--- SharePoint/Scripts/Restart SharePoint Services.cmd
+++ SharePoint/Scripts/Restart SharePoint Services.cmd
@@ -1,6 +1,10 @@
 @echo off
 
-@echo Stopping services...
+IF NOT EXIST "%ProgramFiles%\Common Files\Microsoft Shared\web server extensions\12" goto RestartSharePoint2010Services
+
+:RestartSharePoint2007Services
+
+@echo Stopping SharePoint 2007 services...
 
 iisreset /stop /noforce
 
@@ -16,7 +20,7 @@ net stop "Windows SharePoint Services Tracing"
 
 @pause
 
-@echo Starting services...
+@echo Starting SharePoint 2007 services...
 
 net start "Windows SharePoint Services Tracing"
 
@@ -31,3 +35,45 @@ net start "Windows SharePoint Services Timer"
 iisreset /start
 
 @pause
+
+IF NOT EXIST "%ProgramFiles%\Common Files\Microsoft Shared\web server extensions\14" goto End
+
+:RestartSharePoint2010Services
+
+@echo Stopping SharePoint 2010 services...
+
+iisreset /stop /noforce
+
+net stop "SharePoint 2010 User Code Host"
+
+net stop "SharePoint 2010 Timer"
+
+net stop "SharePoint 2010 Administration"
+
+net stop "SharePoint Server Search 14"
+
+net stop "SharePoint Foundation Search V4"
+
+net stop "SharePoint 2010 Tracing"
+
+@pause
+
+@echo Starting SharePoint 2010 services...
+
+net start "SharePoint 2010 Tracing"
+
+net start "SharePoint Foundation Search V4"
+
+net start "SharePoint Server Search 14"
+
+net start "SharePoint 2010 Administration"
+
+net start "SharePoint 2010 Timer"
+
+net start "SharePoint 2010 User Code Host"
+
+iisreset /start
+
+@pause
+
+:End
